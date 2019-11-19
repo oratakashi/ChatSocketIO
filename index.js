344f387066b6829237df44127a0c39be59deab5c
Oratakashi Nhamako <davidcreazy@gmail.com>
2019-11-19 15:48:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,66 @@
 const express = require('express'),
-http = require('http'),
-app = express(),
-server = http.createServer(app),
-io = require('socket.io').listen(server);
+    http = require('http'),
+    app = express(),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
 app.get('/', (req, res) => {
 
-res.send('Chat Server is running on port 3000')
+    res.send('Chat Server is running on port 3000')
 });
+io.on('connection', (socket) => {
+
+    console.log('user connected')
+
+    socket.on('join', function (userNickname) {
+
+        console.log(userNickname + " : has joined the chat ");
+
+        socket.broadcast.emit('userjoinedthechat', userNickname + " : has joined the chat ");
+    })
 
 
-server.listen(3000, "192.168.1.8", ()=>{
+    socket.on('messagedetection', (senderNickname, messageContent) => {
 
-console.log('Node app is running on port 3000')
+        //log the message in console 
 
-});
+        console.log(senderNickname + " : " + messageContent)
+
+        //create a message object 
+
+        let message = {
+            "message": messageContent,
+            "senderNickname": senderNickname
+        }
+
+        // send the message to all users including the sender  using io.emit() 
+
+        io.emit('message', message)
+
+    })
+
+    socket.on('disconnect', function () {
+
+        console.log(userNickname + ' has left ')
+
+        socket.broadcast.emit("userdisconnect", ' user has left')
+
+
+
+
+    })
+
+
+
+
+})
+
+
+
+
+
+
+server.listen(3000, "192.168.1.8", () => {
+
+    console.log('Node app is running on port 3000')
+
+})
